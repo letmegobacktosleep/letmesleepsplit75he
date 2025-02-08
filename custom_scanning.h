61df2fef71cd45b471142b92ea5534d81d345a9f
letmegobacktosleep <winstonkwan06@gmail.com>
2025-02-09 02:04:39 +1100

    fix function prototype for actuation

--- custom_scanning.h
+++ custom_scanning.h
@@ -15,4 +15,4 @@
 // Function prototypes
 void multiplexer_init(void);
 bool select_multiplexer_channel(uint8_t channel);
-bool actuation(struct key_settings *key, uint8_t current, uint8_t max_key_displacement);
+bool actuation(analog_config_t *config, analog_key_t *key, matrix_row_t *current_row, uint8_t current_col, uint8_t current, uint8_t max_key_displacement);
