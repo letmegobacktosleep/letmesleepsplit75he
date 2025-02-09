1cea59748846d642f64b52d0a14072771cd4734f
letmegobacktosleep <winstonkwan06@gmail.com>
2025-02-09 21:01:55 +1100

    aaasaragrhjrgfjhgfjsdgfsdjf

--- custom_calibration.c
+++ custom_calibration.c
@@ -21,9 +21,9 @@ uint8_t analog_to_distance(uint16_t adc, lookup_table_t *lut_params) {
 }
 
 uint16_t rest_to_absolute_change(uint16_t adc, lookup_table_t *lut_params) {
-    double intermediate = lut_params->lut_a * exp(lut_params->lut_b * distance + lut_params->lut_c) + lut_params->lut_d;
-    uint16_t adc = (uint16_t) MAX(0, MIN(intermediate, lut_params->max_input));
-    return adc;
+    double intermediate = lut_params->lut_a * exp(lut_params->lut_b * adc + lut_params->lut_c) + lut_params->lut_d;
+    uint16_t distance = (uint16_t) MAX(0, MIN(intermediate, lut_params->max_input));
+    return distance;
 }
 
 uint16_t scale_raw_value(uint16_t raw, uint8_t rest, uint16_t *lut_multiplier){
