aad72d87a1ecf4064700bd3df7f3ac081bd346a8
letmegobacktosleep <winstonkwan06@gmail.com>
2025-02-09 20:37:48 +1100

    fix multiplier pointer in fn prototype

--- custom_calibration.h
+++ custom_calibration.h
@@ -5,4 +5,4 @@
 uint16_t distance_to_analog(uint8_t distance, lookup_table_t *lut_params);
 uint8_t analog_to_distance(uint16_t adc, lookup_table_t *lut_params);
 uint8_t rest_to_absolute_change(uint16_t adc, lookup_table_t *lut_params);
-uint16_t scale_raw_value(uint16_t raw, uint8_t rest, lookup_table_t *multiplier);
+uint16_t scale_raw_value(uint16_t raw, uint8_t rest, uint16_t *lut_multiplier);
