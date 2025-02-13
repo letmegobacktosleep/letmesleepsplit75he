bfe0e63a240453c78ad89f17a6d7f10131200bb6
letmegobacktosleep <winstonkwan06@gmail.com>
2025-02-13 15:50:28 +1100

    add non-vial keymap

---
+++ keymaps/default/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4C, 0x14, 0x12, 0x05, 0x00, 0xDC, 0xBA, 0xF4}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
