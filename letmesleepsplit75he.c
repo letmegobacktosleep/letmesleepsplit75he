e4d382c64c5a4bb701594acf18ed36f9b2639be8
letmegobacktosleep <winstonkwan06@gmail.com>
2025-03-09 01:39:26 +1100

    initial attempt at custom menu in vial

--- letmesleepsplit75he.c
+++ letmesleepsplit75he.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#include "quantum.h"
 #include "transactions.h"
 #include "pointing_device.h"
 
@@ -215,11 +216,11 @@ void bootmagic_scan(void) {
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     switch (keycode){
         case KC_ESC:
-            // press caps instead of esc if caps is on
+            // press caps if caps is on
             if (host_keyboard_led_state().caps_lock && record->event.pressed){
                 tap_code(KC_CAPS);
-                return false;
             }
+            return false;
 
 #    ifdef ANALOG_KEY_VIRTUAL_AXES
         // check for keycodes which toggle joystick or mouse
@@ -549,35 +550,301 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 #if defined(VIA_ENABLE)
 
 enum letmesleep_key_config {
-	id_mode = 1,
-	id_actuation_point,
-	id_deadzone,
-	id_down,
-	id_up,
+	id_key_mode = 1,
+	id_key_actuation_point,
+	id_key_deadzone,
+	id_key_down,
+	id_key_up,
 };
 
 # if defined(VIAL_ENABLE)
 
-void raw_hid_receive_kb(uint8_t *data, uint8_t length) {
-    uint8_t *command_id = &(data[0]);
+enum letmesleep_cmd {
+    id_custom_get_key_config = 1,
+    id_custom_set_key_config,
+    id_custom_get_lut_config,
+    id_custom_set_lut_config,
+};
 
-    // Vial uses an older version of via.c
-    // whidh does not have "via_custom_value_command_kb"
-    // use "id_unhandled" to invoke "via_custom_value_command_kb"
-    if (*command_id == id_unhandled) {
-        letmesleep_custom_command_kb(&data[1], length - 1);
+enum letmesleep_lut_id {
+    id_lut_multiplier = 1,
+    id_lut_displacement,
+    id_lut_joystick,
+};
+
+enum letmesleep_lut_config {
+    id_lut_a = 1,
+    id_lut_b,
+    id_lut_c,
+    id_lut_d,
+    id_lut_max_input,
+    id_lut_max_output,
+};
+
+void letmesleep_get_key_config(uint8_t *data){
+    uint8_t *value_id   = &(data[0]);
+    uint8_t *row        = &(data[1]);
+    uint8_t *col        = &(data[2]);
+    uint8_t *value_data = &(data[3]);
+
+    switch (*value_id) {
+        case id_key_mode:
+            *value_data = analog_config[*row][*col].mode;
+            break;
+        case id_key_actuation_point:
+            *value_data = analog_config[*row][*col].lower;
+            break;
+        case id_key_deadzone:
+            *value_data = analog_config[*row][*col].upper;
+            break;
+        case id_key_down:
+            *value_data = analog_config[*row][*col].down;
+            break;
+        case id_key_up:
+            *value_data = analog_config[*row][*col].up;
+            break;
+        default:
+            break;
+    }
+}
+
+void letmesleep_set_key_config(uint8_t *data){
+    uint8_t *value_id   = &(data[0]);
+    uint8_t *row        = &(data[1]);
+    uint8_t *col        = &(data[2]);
+    uint8_t *value_data = &(data[3]);
+
+    switch (*value_id) {
+        case id_key_mode:
+            analog_key[*row][*col].mode = *value_data;    
+            analog_config[*row][*col].mode = *value_data;
+            break;
+        case id_key_actuation_point:
+            analog_config[*row][*col].lower = *value_data;
+            break;
+        case id_key_deadzone:
+            analog_config[*row][*col].upper = *value_data;
+            break;
+        case id_key_down:
+            analog_config[*row][*col].down = *value_data;
+            break;
+        case id_key_up:
+            analog_config[*row][*col].up = *value_data;
+            break;
+        default:
+            break;
+    }
+}
+
+void letmesleep_get_lut_config(uint8_t *data){
+    uint8_t *value_id   = &(data[0]);
+    uint8_t *lut_id     = &(data[1]);
+    double  *value_data = (double *) &(data[2]);
+
+    lookup_table_t *lut_config = NULL;
+    switch (*lut_id) {
+        case id_lut_multiplier:
+            lut_config = &calibration_parameters.multiplier;
+            break;
+        case id_lut_displacement:
+            lut_config = &calibration_parameters.displacement;
+            break;
+        case id_lut_joystick:
+            lut_config = &calibration_parameters.joystick;
+            break;
+        default:
+            break;
+    }
+
+    switch (*value_id) {
+        case id_lut_a:
+            *value_data = lut_config->lut_a;
+            break;
+        case id_lut_b:
+            *value_data = lut_config->lut_b;
+            break;
+        case id_lut_c:
+            *value_data = lut_config->lut_c;
+            break;
+        case id_lut_d:
+            *value_data = lut_config->lut_d;
+            break;
+        case id_lut_max_input:
+            *value_data = (double) lut_config->max_input;
+            break;
+        case id_lut_max_output:
+            *value_data = (double) lut_config->max_output;
+            break;
+        default:
+            break;
+    }
+}
+
+void letmesleep_set_lut_config(uint8_t *data){
+    uint8_t *value_id   = &(data[0]);
+    uint8_t *lut_id     = &(data[1]);
+    double  *value_data = (double *) &(data[2]);
+    
+    lookup_table_t *lut_config = NULL;
+    switch (*lut_id) {
+        case id_lut_multiplier:
+            lut_config = &calibration_parameters.multiplier;
+            break;
+        case id_lut_displacement:
+            lut_config = &calibration_parameters.displacement;
+            break;
+        case id_lut_joystick:
+            lut_config = &calibration_parameters.joystick;
+            break;
+        default:
+            break;
+    }
+
+    switch (*value_id) {
+        case id_lut_a:
+            lut_config->lut_a = *value_data;
+            break;
+        case id_lut_b:
+            lut_config->lut_b = *value_data;
+            break;
+        case id_lut_c:
+            lut_config->lut_c = *value_data;
+            break;
+        case id_lut_d:
+            lut_config->lut_d = *value_data;
+            break;
+        case id_lut_max_input:
+            lut_config->max_input = (uint8_t) *value_data;
+            break;
+        case id_lut_max_output:
+            lut_config->max_output = (uint8_t) *value_data;
+            break;
+        default:
+            break;
     }
 }
 
 void letmesleep_custom_command_kb(uint8_t *data, uint8_t length){
-    
+    /* data = [ command_id, channel_id, custom_data ] */
+    uint8_t *command_id  = &(data[0]);
+    uint8_t *channel_id  = &(data[1]);
+    uint8_t *custom_data = &(data[2]);
+
+    if (*channel_id == id_custom_channel) {
+        switch (*command_id) {
+            case id_custom_get_key_config: {
+                letmesleep_get_key_config(custom_data);
+                break;
+            }
+            case id_custom_set_key_config: {
+                letmesleep_set_key_config(custom_data);
+                break;
+            }
+            case id_custom_get_lut_config: {
+				letmesleep_get_lut_config(custom_data);
+                break;
+            }
+            case id_custom_set_lut_config: {
+                letmesleep_set_lut_config(custom_data);
+                break;
+            }
+            default: {
+                /* Unhandled message */
+                *command_id = id_unhandled;
+                break;
+            }
+        }
+        return;
+    }
+
     /* Return the unhandled state */
 	*command_id = id_unhandled;
 
 	/* DO NOT call raw_hid_send(data,length) here, let caller do this */
 }
 
+void raw_hid_receive_kb(uint8_t *data, uint8_t length) {
+    uint8_t *command_id = &(data[0]);
+
+    // Vial uses an older version of via.c
+    // whidh does not have "via_custom_value_command_kb"
+    // use "id_unhandled" to invoke "letmesleep_custom_command_kb"
+    if (*command_id == id_unhandled) {
+        letmesleep_custom_command_kb(&data[1], length - 1);
+    }
+}
+
 # else
+
+void via_config_set_value(uint8_t *data) {
+    /* data = [ value_id, value_data ] */
+    uint8_t *value_id   = &(data[0]);
+    uint8_t *value_data = &(data[1]);
+
+    // loop through rows
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++){
+        // only run if row isn't DKS
+        if (
+# ifdef SPLIT_KEYBOARD
+            (row != MATRIX_ROWS   - 1) && // last row on right
+            (row != ROWS_PER_HAND - 1) // last row on left
+# else
+            (row != MATRIX_ROWS   - 1) && // last row
+            (row != MATRIX_ROWS   - 2) // second last row
+# endif
+        )
+        {   
+            // loop through columns
+            for (uint8_t col = 0; col < MATRIX_COLS; col++){
+                switch (*value_id) {
+                    case id_key_mode:
+                        analog_key[row][col].mode = *value_data;    
+                        analog_config[row][col].mode = *value_data;
+                        break;
+                    case id_key_actuation_point:
+                        analog_config[row][col].lower = *value_data;
+                        break;
+                    case id_key_deadzone:
+                        analog_config[row][col].upper = *value_data;
+                        break;
+                    case id_key_down:
+                        analog_config[row][col].down = *value_data;
+                        break;
+                    case id_key_up:
+                        analog_config[row][col].up = *value_data;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
+
+void via_config_get_value(uint8_t *data) {
+    uint8_t *value_id   = &(data[0]);
+    uint8_t *value_data = &(data[1]);
+
+    switch (*value_id) {
+        case id_key_mode:
+            *value_data = analog_config[0][0].mode;
+            break;
+        case id_key_actuation_point:
+            *value_data = analog_config[0][0].lower;
+            break;
+        case id_key_deadzone:
+            *value_data = analog_config[0][0].upper;
+            break;
+		case id_key_down:
+            *value_data = analog_config[0][0].down;
+            break;
+		case id_key_up:
+            *value_data = analog_config[0][0].up;
+            break;
+		default:
+			break;
+	}
+}
 
 void via_custom_value_command_kb(uint8_t *data, uint8_t length){
     /* data = [ command_id, channel_id, value_id, value_data ] */
@@ -614,75 +881,6 @@ void via_custom_value_command_kb(uint8_t *data, uint8_t length){
 	/* DO NOT call raw_hid_send(data,length) here, let caller do this */
 }
 
-void via_config_set_value(uint8_t *data) {
-    /* data = [ value_id, value_data ] */
-    uint8_t *value_id   = &(data[0]);
-    uint8_t *value_data = &(data[1]);
-
-    // loop through rows
-    for (uint8_t row = 0; row < MATRIX_ROWS; row++){
-        // only run if row isn't DKS
-        if (
-# ifdef SPLIT_KEYBOARD
-            (row != MATRIX_ROWS   - 1) && // last row on right
-            (row != ROWS_PER_HAND - 1) // last row on left
-# else
-            (row != MATRIX_ROWS   - 1) && // last row
-            (row != MATRIX_ROWS   - 2) // second last row
 # endif
-        )
-        {   
-            // loop through columns
-            for (uint8_t col = 0; col < MATRIX_COLS; col++){
-                switch (*value_id) {
-                    case id_mode:
-                        analog_key[row][col].mode = *value_data;    
-                        analog_config[row][col].mode = *value_data;
-                        break;
-                    case id_actuation_point:
-                        analog_config[row][col].lower = *value_data;
-                        break;
-                    case id_deadzone:
-                        analog_config[row][col].upper = *value_data;
-                        break;
-                    case id_down:
-                        analog_config[row][col].down = *value_data;
-                        break;
-                    case id_up:
-                        analog_config[row][col].up = *value_data;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-    }
-}
 
-void via_config_get_value(uint8_t *data) {
-    uint8_t *value_id   = &(data[0]);
-    uint8_t *value_data = &(data[1]);
-
-    switch (*value_id) {
-        case id_mode:
-            *value_data = analog_config[0][0].mode;
-            break;
-        case id_actuation_point:
-            *value_data = analog_config[0][0].lower;
-            break;
-        case id_deadzone:
-            *value_data = analog_config[0][0].upper;
-            break;
-		case id_down:
-            *value_data = analog_config[0][0].down;
-            break;
-		case id_up:
-            *value_data = analog_config[0][0].up;
-            break;
-		default:
-			break;
-	}
-}
-
-# endif
 #endif
